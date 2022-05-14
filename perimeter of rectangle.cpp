b0668c269b41582ab7b0fa6eb41a6b50c9e27124
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-15 02:39:23 +0530

    Create perimeter of rectangle.cpp

---
+++ perimeter of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"L=Length,B=breadth"<<endl;
+    int L=24;
+    int B=22;
+    cout<<"perimeter of rectangle=2(L+B)"<<endl;
+    cout<<"Let Perimeter of Rectangle=P"<<endl;
+    int P=2*(L+B);
+    cout<<P;
+}
