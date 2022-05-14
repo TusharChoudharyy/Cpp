84263b21ce12c8aea184c13f76ce1ebb00a7a664
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-15 02:40:52 +0530

    Create dividing two int.cpp

---
+++ dividing two int.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"dividing a/b"<<endl;
+    int c=a/b;
+    cout<<"the quotient of "<<a<<"and"<<b<<"is"<<c;
+}
+
