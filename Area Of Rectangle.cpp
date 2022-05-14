e554b393d2a3c35327dc78519757eea886b28dd7
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-15 02:35:26 +0530

    Create Area Of Rectangle.cpp

---
+++ Area Of Rectangle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"L=length,B=breadth"<<endl;
+    int L=24;
+    int B=22;
+    cout<<"area of rectangle=L*B"<<endl;
+    cout<<"Let A= area of rectangle(L*B)"<<endl;
+    int A=L*B;
+    cout<<A<<endl;
+}
