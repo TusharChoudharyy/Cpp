a1d4943da6d3056bf3b71cf6d0fa4adff0bbd558
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-16 17:35:51 +0530

    Create calculating aggregate marks.cpp

---
+++ calculating aggregate marks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	int m1,m2,m3,m4,m5,aggr;
+	float per;
+	cout<<"enter marks in 5 sunjects:"<<endl;
+	cin>>m1>>m2>>m3>>m4>>m5;
+	aggr=m1+m2+m3+m4+m5;
+	per=aggr/5;
+	cout<<"aggr marks= "<<aggr<<endl;
+	cout<<"percentage marks="<<per<<endl;
+	
+	
+}
+	
