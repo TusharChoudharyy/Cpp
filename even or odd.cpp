8d4974f0cceff3d19545a06ee98949def540037d
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-16 16:08:03 +0530

    Create even or odd.cpp

---
+++ even or odd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	cout<<"enter the no.= "<<endl;
+	cin>>a;
+	
+if (a%2==0){
+	cout<<"number is even= "<<endl;
+}
+else{
+	cout<<"number is odd";
+}
+}
