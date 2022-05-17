04da3b13cca2a2b1f9a3b5f5611d32809c72ea82
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-17 14:56:01 +0530

    Create check if numbers are equal.cpp

---
+++ check if numbers are equal.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+    if(n1==n2)
+    {
+    cout<<"numbers are equal"<<endl;
+    }
+    else {
+        cout<<"numbers are not equal";
+    }
+}
+                                                                                                         
