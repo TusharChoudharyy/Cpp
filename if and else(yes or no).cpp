15c91c355606e6b77af218fc1b6077d44c5a2a68
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-17 13:10:28 +0530

    Create if and else(yes or no).cpp

---
+++ if and else(yes or no).cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int y;
+    cin>>y;
+    if(y==1){
+        cout<<"yes"<<endl;
+    
+    }
+    else{
+        cout<<"no";
+    }
+}
