1393a91c123032601a89a1c4519ded9d6b9d4bef
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-17 13:13:21 +0530

    Create reversing number.cpp

---
+++ reversing number.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,d5,d4,d3,d2,d1;
+    int revnum;
+    cout<<"enter a five digit number(less than 32767): "<<endl;
+    cin>>n;
+    d5=n%10;
+    n=n/10;
+    d4=n%10;
+    n=n/10;
+    d3=n%10;
+    n=n/10;
+    d2=n%10;
+    n=n/10;
+    d1=n%10;
+    revnum= d5*10000+d4*1000+d3*100+d2*10+d1;
+    cout<<"the reversed number= "<<revnum;
+ 
+
+}
