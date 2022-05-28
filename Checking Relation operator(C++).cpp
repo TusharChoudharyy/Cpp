40a07366a368cc3215b0e949c9ca13a1b1ae6e75
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-28 22:05:08 +0530

    Create Checking Relation operator(C++).cpp

---
+++ Checking Relation operator(C++).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a=2<3;
+    cout<<a<<endl;
+    a=2>3;
+    cout<<a<<endl;
+    a=2==3;
+    cout<<a<<endl;
+    a=2!=3;
+    cout<<a<<endl;
+    a=2<=3;
+    cout<<a<<endl;
+    a=2>=3;
+    cout<<a<<endl;
+}
