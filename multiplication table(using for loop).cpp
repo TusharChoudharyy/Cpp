0fe81fdbf0fdae16d00575cf69236d35bd6261c0
TusharChoudharyy <104813327+TusharChoudharyy@users.noreply.github.com>
2022-05-31 16:55:33 +0530

    Create multiplication table(using for loop).cpp

---
+++ multiplication table(using for loop).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Multiplication table"<<endl;
+    cin>>m;
+    for (int i=1; i<=10; i++){
+        cout<<m<<" * "<<i<<" = "<<m*i<<endl;
+    }
+}
+
